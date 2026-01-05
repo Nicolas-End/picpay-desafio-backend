c92cfe2b120efc84185fb74d470a458f85c670e1
Nicolas-End <nicolasvalsi203.com>
2026-01-05 16:31:26 -0300

    Add the spring boot configuration

---
+++ src/main/java/com/solution/picpay_problem/PicpayProblemApplication.java
@@ -0,0 +1,13 @@
+package com.solution.picpay_problem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayProblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayProblemApplication.class, args);
+	}
+
+}
